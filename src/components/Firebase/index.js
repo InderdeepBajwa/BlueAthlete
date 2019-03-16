a5d81274d051dadebfcccac311bb46b18678fc23
Inderdeep Singh Bajwa <InderdeepBajwa@users.noreply.github.com>
2019-03-16 19:52:46 -0400

    SignIn implemented

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,7 @@
+import FirebaseContext, { withFirebase } from './context';
+import Firebase from './firebase';
+
+// Exports Firebase class, Firebase context including Consumer and Provider component
+export default Firebase;
+
+export { FirebaseContext, withFirebase };
